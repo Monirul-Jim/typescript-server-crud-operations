2ff4f168cde18f9e2b36ee52565eba3fa02d5f0b
unknown <Monirul-Jim>
2023-11-23 02:33:53 +0600

    added eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
-export default app;
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
+// import { userRoutes } from "./app/module/student/user.route";
+
+app.use(express.json())
+app.use(cors())
+// app.use("/api/v1/students", userRoutes);
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World!')
+}
+app.get('/', getAController)
+export default app
--- src/app/module/student/user.route.ts
+++ src/app/module/student/user.route.ts
@@ -3,9 +3,3 @@ import { StudentController } from "./user.controll";
 const router = express.Router();
 router.post("/api/users", StudentController.createUserInfo);
 export const userRoutes = router;
-
-// import express from 'express'
-// import { StudentController } from './student.controller'
-// const router = express.Router()
-// router.post('/create-student', StudentController.createStudent)
-// export const StudentRoutes = router
